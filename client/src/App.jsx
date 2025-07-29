61e9e27a3f001fb29317488c22fbb15e5b0ccf7b
Bereket <bereketdesalegn097@gmail.com>
2025-07-29 18:22:00 +0300

    initial setup for react tailwind with prettier

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center text-2xl tracking-wider hover:text-6xl">
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
