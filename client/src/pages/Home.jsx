e449b10e51618ed0153ee1d4116253a55d700026
Bereket <bereketdesalegn097@gmail.com>
2025-07-30 22:12:22 +0300

    initial home page ui

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <div className="relative min-h-screen w-screen bg-[radial-gradient(circle_600px_at_center_120%,_#30B2AD_0%,_#80AFFB_30%,_#BFD4FA_70%,_#FFFFFF_100%)]">
+      <Header />
+    </div>
+  )
+}
+
+export default Home
