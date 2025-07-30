ef086d648435d3b9d1e0df2881e71aae68ad6c87
Bereket <bereketdesalegn097@gmail.com>
2025-07-30 22:10:34 +0300

    initial navbar ui

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  const navList = [
+    { name: 'Home', to: '/' },
+    { name: 'Quiz', to: '/quiz' },
+    { name: 'Leader Board', to: '/leaderboard' },
+  ]
+  return (
+    <nav className="flex gap-1 text-lg font-medium tracking-wide">
+      {navList.map((navItem, index) => (
+        <Link key={index} to={navItem.to} className="group">
+          <span className="group-active:text-primary group-hover:decoration-primary me-2 group-hover:underline last:me-0">
+            {navItem.name}
+          </span>
+          {index !== navList.length - 1 && <span>{'>'}</span>}
+        </Link>
+      ))}
+    </nav>
+  )
+}
+
+export default Navbar
