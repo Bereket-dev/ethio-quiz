f959069a6ee5a1d1e09b3f5b8224fb474e546a84
Bereket <bereketdesalegn097@gmail.com>
2025-07-31 22:33:25 +0300

    initial quizkingdom card ui

---
+++ client/src/components/QuizKingdomCard.jsx
@@ -0,0 +1,20 @@
+function QuizKingdomCard({ image, title, description }) {
+  return (
+    <div className="w-[330px] rounded-xl bg-white px-6 py-[21px] text-black shadow-xl shadow-gray-200">
+      <div className="flex items-center pb-[19px]">
+        <div className="me-[21px] flex h-[60px] w-[60px] items-center overflow-hidden rounded-full">
+          <img
+            src={image.src}
+            alt={`${image.label} icon`}
+            className="w-full object-cover object-center"
+          />
+        </div>
+
+        <h3 className="text-xl font-medium tracking-wide">{title}</h3>
+      </div>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default QuizKingdomCard
