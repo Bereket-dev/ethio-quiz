0fb7016990863b4565e91970d811c933d35c502d
Bereket <bereketdesalegn097@gmail.com>
2025-08-07 01:40:25 +0300

    initial user schema

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profileImage: { default: null },
+  score: { type: Number },
+  quizzesTaken: { type: Number },
+});
+
+module.exports = mongoose.model("User", userSchema);
