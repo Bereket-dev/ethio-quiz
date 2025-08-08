baf538ae523e647ffcf14004f4acc1660b1d6378
Bereket <bereketdesalegn097@gmail.com>
2025-08-08 08:49:52 +0300

    update to handle in undifined props case

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,11 @@
-function Banner({ title, description, image, cta = null }) {
+import { Link } from 'react-router-dom'
+
+function Banner({ title, description, image, cta }) {
   return (
     <div
       className="mx-auto mt-14 flex h-full flex-col items-center justify-center gap-8 px-6 md:max-w-6xl md:flex-row md:justify-between md:gap-14 md:px-14"
       style={
-        image.direction === 'left'
+        image?.direction === 'left'
           ? { flexDirection: 'row-reverse' }
           : undefined
       }
@@ -15,16 +17,19 @@ function Banner({ title, description, image, cta = null }) {
         <p className="text-base font-normal text-gray-600 md:text-lg">
           {description}
         </p>
-        {cta && (
-          <button className="bg-primary hover:bg-primary-dark mt-4 rounded-lg px-5 py-2 text-sm font-medium text-white transition md:text-base">
-            {cta}
-          </button>
+        {cta?.to && cta?.text && (
+          <Link to={cta.to}>
+            {' '}
+            <button className="bg-primary hover:bg-primary-dark mt-4 rounded-lg px-5 py-2 text-sm font-medium text-white transition md:text-base">
+              {cta.text}
+            </button>
+          </Link>
         )}
       </div>
       <div className="animate-fade-up delay-100">
         <img
-          src={image.src}
-          alt={image.alt}
+          src={image?.src}
+          alt={image?.alt || 'Banner image'}
           className="w-[250px] object-contain md:h-[400px] md:w-auto"
         />
       </div>
