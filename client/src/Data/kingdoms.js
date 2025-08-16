56e3104c9350e8f1f4791bae3b9b7eff36e74e76
Bereket <bereketdesalegn097@gmail.com>
2025-08-16 18:39:21 +0300

    admin add kingdom field data

---
+++ client/src/Data/kingdoms.js
@@ -0,0 +1,16 @@
+export const kingdomsAddFormFields = [
+  {
+    type: 'text',
+    label: 'Kingdom Name',
+    name: 'title',
+    placeholder: 'Enter kingdom name',
+    required: true,
+  },
+  {
+    type: 'textarea',
+    label: 'Kingdom Description',
+    name: 'description',
+    placeholder: 'Enter kingdom description',
+    required: true,
+  },
+]
