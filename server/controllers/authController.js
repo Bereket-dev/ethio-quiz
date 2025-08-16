aaebca206b362c306aa44d83b7cb8913ca2818f7
Bereket <bereketdesalegn097@gmail.com>
2025-08-16 18:54:53 +0300

    initial authentication controllers - signup, login and logout

---
+++ server/controllers/authController.js
@@ -0,0 +1,50 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const signUpUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, email, password: hashed });
+
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res
+        .status(400)
+        .json({ error: "Email or username already exists" });
+    }
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ error: "User not found!" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ error: "Invalid Credentials!" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "strict",
+    secure: false, //true for production
+  });
+
+  res.json({
+    message: "Logged in successfully",
+    user: { id: user._id, username: user.username },
+  });
+};
+
+const logoutUser = async (req, res) => {
+  res.clearCookie("token").json({ message: "Logged out!" });
+};
+
+module.exports = { signUpUser, loginUser, logoutUser };
