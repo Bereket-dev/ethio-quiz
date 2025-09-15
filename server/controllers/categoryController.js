5543aee37a6b31860bfadc440a74530a495e6a72
Bereket <bereketdesalegn097@gmail.com>
2025-09-15 11:56:20 +0300

    based on form field name change on kingdom to kingdomId

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,44 +1,141 @@
 const { Category } = require("../models/quizModels");
+const cloudinary = require("../config/cloudinary");
 
 const createNewCategory = async (req, res) => {
-  const { title, img_src, color } = req.body;
+  const {
+    title,
+    description,
+    points,
+    timeAllowed,
+    color,
+    kingdomId,
+  } = req.body;
+  const file = req.file;
+
+  if (!title) return res.status(400).json({ error: "A title is required" });
+  if (!description)
+    return res.status(400).json({ error: "A description is required" });
+  if (!points) return res.status(400).json({ error: "Points are required" });
+  if (!timeAllowed)
+    return res.status(400).json({ error: "A timeAllowed is required" });
+  if (!color) return res.status(400).json({ error: "A color is required" });
+  if (!kingdomId)
+    return res.status(400).json({ error: "A kingdomId is required" });
+
   try {
-    const image = {
-      src: img_src,
-      alt: `${title} image`,
-    };
-    const category = await Category.create({ title, image, color });
-    res.status(201).json({ message: "Category created successfully" });
+    let image = null;
+    if (file) {
+      const result = await cloudinary.uploader.upload(file.path);
+      image = {
+        src: result.secure_url,
+        alt: `${title} kingdom image`,
+      };
+    } else {
+      return res.status(400).json({ error: "Image is required" });
+    }
+
+    const category = await Category.create({
+      title,
+      image,
+      description,
+      points,
+      timeAllowed,
+      color,
+      kingdomId,
+    });
+    res.status(201).json(category);
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Failed to create new category", error: error.message });
+    if (error.code === 11000) {
+      return res
+        .status(400)
+        .json({ error: "Category with this title already exists" });
+    }
+    res.status(500).json({ error: "Failed to create new category" });
+  }
+};
+
+const editCategory = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    description,
+    points,
+    timeAllowed,
+    color,
+    kingdomId,
+  } = req.body;
+  const file = req.file;
+
+  if (!title) return res.status(400).json({ error: "A title is required" });
+  if (!description)
+    return res.status(400).json({ error: "A description is required" });
+  if (!points) return res.status(400).json({ error: "Points are required" });
+  if (!timeAllowed)
+    return res.status(400).json({ error: "A timeAllowed is required" });
+  if (!color) return res.status(400).json({ error: "A color is required" });
+  if (!kingdomId)
+    return res.status(400).json({ error: "A kingdomId is required" });
+
+  try {
+    const updateData = {
+      title,
+      description,
+      points,
+      timeAllowed,
+      color,
+      kingdomId,
+    };
+    let image = null;
+    if (file) {
+      const result = await cloudinary.uploader.upload(file.path);
+      image = {
+        src: result.secure_url,
+        alt: `${title} image`,
+      };
+    }
+
+    if (image) updateData.image = image;
+
+    const category = await Category.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+    res.status(201).json(category);
+  } catch (error) {
+    if (error.code === 11000) {
+      return res
+        .status(400)
+        .json({ error: "Category with this title already exists" });
+    }
+    res.status(500).json({ error: "Failed to update category" });
   }
 };
 
 const removeCategory = async (req, res) => {
+  const { id } = req.params;
   try {
-    const result = await Category.findOneAndDelete({ title });
-    if (!result)
-      return res.status(404).json({ message: "Category not found!" });
-    res.status(201).json({ message: "Category removed successfully!" });
+    const result = await Category.findByIdAndDelete(id);
+    if (!result) return res.status(404).json({ error: "Category not found!" });
+
+    res.status(200).json({ message: "Category removed successfully!" });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Failed to delete category!", error: error.message });
+    res.status(500).json({ error: "Failed to remove category" });
+  }
+};
+const getAllCategories = async (req, res) => {
+  try {
+    const allCategories = await Category.find();
+    res.status(200).json({
+      message: "All categories fetched successfully",
+      categories: allCategories,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get all category" });
   }
 };
 
-const getCategoriesByKingdom = async (req, res) => {
-  try {
-    const allCategories = await Category.findMany();
-    res.status(201).json({ message: "All kingdoms getted successfully" });
-    return allCategories;
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Failed to get all kingdom", error: error.message });
-  }
+module.exports = {
+  createNewCategory,
+  editCategory,
+  removeCategory,
+  getAllCategories,
 };
-
-module.exports = { createNewCategory, removeCategory, getCategoriesByKingdom };
