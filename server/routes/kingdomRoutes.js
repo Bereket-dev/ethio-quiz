57b4acf0e90913d439de9d04c3b9f66ad1ef789c
Bereket <bereketdesalegn097@gmail.com>
2025-10-03 16:59:48 +0300

    initial kingdom routes

--- server/routes/kingdomRoutes.js
+++ server/routes/kingdomRoutes.js
@@ -6,13 +6,13 @@ const {
 } = require("../controllers/kingdomController");
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "uploads/kingdom" });
 const express = require("express");
 const router = express.Router();
 
 router.get("/", getAllKingdoms);
-router.delete("/:title", removeKingdom);
 router.post("/create", upload.single("img_icon"), createNewKingdom);
-router.put("/edit", upload.single("img_icon"), editKingdom);
+router.put("/edit/:id", upload.single("img_icon"), editKingdom);
+router.delete("/remove/:id", removeKingdom);
 
 module.exports = router;
