cac4f6effaee368f24954c6586a9b83839f0af32
Bereket <bereketdesalegn097@gmail.com>
2025-10-03 16:43:19 +0300

    working services of question to find by category, add, edit and delete

---
+++ client/src/services/questionServices.js
@@ -0,0 +1,85 @@
+export const getQuestionsByCategory = async (categoryId) => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/questions/category/${categoryId}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      },
+    )
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.error || 'Failed to fetch questions of a category')
+    }
+
+    return data || []
+  } catch (err) {
+    throw err
+  }
+}
+
+export const addOneQuestion = async (formData) => {
+  const reponse = await fetch('http://localhost:5000/api/questions/create', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  })
+  const result = await reponse.json()
+
+  if (!reponse.ok) {
+    throw new Error(result.error || 'Failed to add question')
+  }
+  return result
+}
+
+export const editOneQuestion = async (formData) => {
+  try {
+    const id = formData._id //kingdom id
+    const response = await fetch(
+      `http://localhost:5000/api/questions/edit/${id}`,
+      {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      },
+    )
+
+    const updatedQuestion = await response.json()
+    if (response.ok) {
+      return updatedQuestion
+    } else {
+      throw new Error(updatedQuestion.error || 'Failed to update question!')
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+export const removeOneQuestion = async (id) => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/questions/remove/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      },
+    )
+
+    const result = await response.json()
+    if (response.ok) {
+      return result.message || `Question deleted successfully!`
+    } else {
+      throw new Error(result.error || 'Failed to delete question')
+    }
+  } catch (err) {
+    throw err
+  }
+}
