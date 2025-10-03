216eab99d95d69a8d2dd62f5514e46d688d37b1f
Bereket <bereketdesalegn097@gmail.com>
2025-10-03 17:03:27 +0300

    connect all routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const quizRoutes = require("./routes/quizRoutes");
 const kingdomRoutes = require("./routes/kingdomRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const questionRoutes = require("./routes/questionRoutes");
 const { authenticateToken, requireAdmin } = require("./middleware/auth");
 
 dotenv.config();
@@ -19,5 +20,6 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use("/api/auth", authRoutes);
-app.use("/api/quiz", authenticateToken, requireAdmin, quizRoutes);
 app.use("/api/kingdom", kingdomRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/questions", questionRoutes);
