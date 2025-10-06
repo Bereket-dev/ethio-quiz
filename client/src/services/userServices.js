16c9b9e6a2a7f36ff29f033daf8b64f911b183fa
Bereket <bereketdesalegn097@gmail.com>
2025-10-06 10:14:56 +0300

    make quiz submittion to store with user id and category id:

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,19 +1,19 @@
-export const updateUserScore = async (userId, score, quizzesTaken) => {
-  const scoreData = { score, quizzesTaken }
+export const updateUserScore = async (userId, categoryId, score) => {
   try {
     const response = await fetch(
-      `http://localhost:5000/api/user/update-score/${userId}`,
+      `http://localhost:5000/api/user/update-score/${userId}/${categoryId}`,
       {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(scoreData),
+        body: JSON.stringify({ score: score }),
+        credentials: 'include',
       },
     )
-    const updatedUserData = await response.json()
+    const updatedQuizResult = await response.json()
     if (response.ok) {
-      return updatedUserData
+      return updatedQuizResult
     } else {
-      throw new Error(updatedUserData.error || 'Failed to update user score!')
+      throw new Error(updatedQuizResult.error || 'Failed to update user score!')
     }
   } catch (err) {
     throw err
