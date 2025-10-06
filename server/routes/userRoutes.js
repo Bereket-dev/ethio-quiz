a7b834db61cb14aff97e958fcf1a33d1b840fc16
Bereket <bereketdesalegn097@gmail.com>
2025-10-06 16:09:02 +0300

    add top-players route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { updateUserScore } = require("../controllers/userController");
+const {
+  updateUserScore,
+  getTopPlayers,
+} = require("../controllers/userController");
 
 router.put("/update-score/:userId/:categoryId", updateUserScore);
+router.get("/top-players", getTopPlayers);
 
 module.exports = router;
