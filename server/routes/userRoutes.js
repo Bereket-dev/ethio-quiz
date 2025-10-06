ebf423a6e4d02fc849b6b4fe3c232b5cd93bd343
Bereket <bereketdesalegn097@gmail.com>
2025-10-06 10:18:59 +0300

    add category id parameter

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { updateUserScore } = require("../controllers/userController");
 
-router.put("/update-score/:userId", updateUserScore);
+router.put("/update-score/:userId/:categoryId", updateUserScore);
 
 module.exports = router;
