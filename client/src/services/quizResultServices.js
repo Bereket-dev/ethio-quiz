421307187e84fdce36453c532dc23bd7ba13d7b4
Bereket <bereketdesalegn097@gmail.com>
2025-10-16 16:55:19 +0300

    add working api to get recently taken quizes

---
+++ client/src/services/quizResultServices.js
@@ -0,0 +1,66 @@
+export const updateUserScore = async (userId, categoryId, score) => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/quiz-result/update-score/${userId}/${categoryId}`,
+      {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ score: score }),
+        credentials: 'include',
+      },
+    )
+    const updatedQuizResult = await response.json()
+    if (response.ok) {
+      return updatedQuizResult
+    } else {
+      throw new Error(updatedQuizResult.error || 'Failed to update user score!')
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+export const getTopPlayers = async () => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/quiz-result/top-players`,
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      },
+    )
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      throw new Error(data.error || 'Failed to fetch top players!')
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+export const getRecentQuizResult = async (userId) => {
+  try {
+    const response = await fetch(
+      `http://localhost:5000/api/quiz-result/recent-activity/${userId}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      },
+    )
+
+    const data = await response.json()
+    if (response.ok) {
+      localStorage.setItem('recent-activity', JSON.stringify(data))
+      return data
+    } else
+      throw new Error(data.error || 'Failed to get the recent quiz results')
+  } catch (err) {
+    throw err
+  }
+}
