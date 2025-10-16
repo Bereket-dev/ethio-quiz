2269a65e01fc63e94738f533b1c480c8a12ac3f0
Bereket <bereketdesalegn097@gmail.com>
2025-10-16 16:54:03 +0300

    dynamic recent activity component specific to user and kingdom

--- client/src/components/user/recentactivity/RecentActivities.jsx
+++ client/src/components/user/recentactivity/RecentActivities.jsx
@@ -1,18 +1,20 @@
 import RecentActivityCard from './RecentActivityCard'
 
-function RecentActivities({ recentActivities }) {
+function RecentActivities({ recentActivities = [], categories = [] }) {
   return (
     <section className="mx-auto mt-24 max-w-6xl px-6 md:px-14">
-      <h2 className="mb-10 text-center text-2xl font-semibold text-gray-800 capitalize md:text-4xl md:tracking-wide">
-        Recent Activity
-      </h2>
+      {recentActivities.length > 0 && (
+        <h2 className="mb-10 text-center text-xl font-semibold text-gray-800 capitalize md:text-4xl md:tracking-wide">
+          Recent Activity
+        </h2>
+      )}
 
       <div className="space-y-4">
         {recentActivities.map((activity, index) => (
           <RecentActivityCard
             key={index}
-            index={activity.index}
-            kingdom={activity.kingdom}
+            recentActivity={activity}
+            categories={categories}
             score={activity.score}
           />
         ))}
--- client/src/components/user/recentactivity/RecentActivityCard.jsx
+++ client/src/components/user/recentactivity/RecentActivityCard.jsx
@@ -1,45 +1,57 @@
 import CategoryCard from '../categories/CategoryCard'
 import DoughnutChart from '../charts/DoughnutChart'
+import { Link } from 'react-router-dom'
 
-const freshmanCategories = []
-const triviaCategories = []
+function RecentActivityCard({ recentActivity, categories = [], score }) {
+  const recentCategory = categories?.find(
+    (cat) => cat._id === recentActivity.categoryId,
+  )
 
-function RecentActivityCard({ index, kingdom, score }) {
-  const isFreshman = kingdom === 'freshman'
-  const category = isFreshman
-    ? freshmanCategories[index]
-    : triviaCategories[index]
-
+  const questions = JSON.parse(localStorage.getItem('questions'))
+  const categoryQuestions = questions
+    ? questions.filter((que) => que?.categoryId === recentCategory?._id)
+    : null
+  const outOf = categoryQuestions ? categoryQuestions?.length : 0
   const cardSize = { cardWidth: '80px', imgWidth: '36px' }
 
+  const dataToPass = {
+    questionPoints: recentCategory?.points,
+    questionTime: recentCategory?.timeAllowed,
+    title: recentCategory?.title,
+  }
   return (
-    <div className="mx-auto flex max-w-2xl items-center justify-between rounded-2xl bg-white px-6 py-4 shadow-xl transition hover:shadow-2xl">
-      {/* Left section: Category info */}
-      <div className="flex items-center gap-4">
-        <CategoryCard
-          image={category.image}
-          color={category.color.lighter}
-          size={cardSize}
-        />
-        <div className="flex flex-col">
-          <h4 className="text-lg font-semibold text-gray-800 capitalize">
-            {category.title}
-          </h4>
-          <p className="text-sm text-gray-500">{score.outOf} questions</p>
+    <Link to={`/quizflow/${recentCategory?._id}`} state={dataToPass}>
+      {' '}
+      <div className="mx-auto flex max-w-2xl items-center justify-between rounded-2xl bg-white px-6 py-4 shadow-xl transition hover:shadow-2xl">
+        {/* Left section: Category info */}
+        <div className="flex items-center gap-4">
+          <CategoryCard
+            image={recentCategory?.image}
+            color={recentCategory?.color}
+            size={cardSize}
+          />
+          <div className="flex flex-col">
+            <h4 className="text-lg font-semibold text-gray-800 capitalize">
+              {recentCategory?.title}
+            </h4>
+            <p className="text-sm text-gray-500">{score} Points</p>
+          </div>
         </div>
-      </div>
 
-      {/* Right section: Doughnut chart */}
-      <div
-        className="relative h-[75px] w-[75px] flex-shrink-0 rounded-full"
-        style={{ backgroundColor: category.color.lighter }}
-      >
-        <DoughnutChart score={score} color={category.color} />
-        <div className="absolute inset-0 flex items-center justify-center text-sm font-semibold text-gray-700">
-          {score.result}/{score.outOf}
+        {/* Right section: Doughnut chart */}
+        <div
+          className="relative h-[75px] w-[75px] flex-shrink-0 rounded-full"
+          style={{ backgroundColor: recentCategory?.color }}
+        >
+          <DoughnutChart score={score} boldColor={recentCategory?.color} />
+          <div className="absolute inset-0 flex items-center justify-center text-sm font-semibold text-gray-800">
+            <div className="rounded-full bg-white/90 p-3">
+              {outOf && `${score / recentCategory?.points} / ${outOf}`}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
