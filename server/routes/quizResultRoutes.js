d1285d5968d81bfc5aed508e85adbaf78e08fe34
Bereket <bereketdesalegn097@gmail.com>
2025-10-17 15:31:26 +0300

    add route to top players stats getting controller

--- server/routes/quizResultRoutes.js
+++ server/routes/quizResultRoutes.js
@@ -5,11 +5,13 @@ const {
   getHighScoreUser,
   getRecentQuizResult,
   getMonthlyQuizStats,
+  getTopPlayersStats,
 } = require("../controllers/quizResultController");
 
 router.put("/update-score/:userId/:categoryId", updateUserScore);
 router.get("/top-players", getHighScoreUser);
 router.get("/recent-activity/:userId", getRecentQuizResult);
 router.get("/monthly-stats", getMonthlyQuizStats);
+router.get("/top-players-stats", getTopPlayersStats);
 
 module.exports = router;
