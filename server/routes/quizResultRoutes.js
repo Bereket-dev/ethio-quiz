ce7b79d0d4009998425284ff2b827e72b8a6ecba
Bereket <bereketdesalegn097@gmail.com>
2025-10-17 10:02:43 +0300

    add route for monthly quiz stats

--- server/routes/quizResultRoutes.js
+++ server/routes/quizResultRoutes.js
@@ -4,10 +4,12 @@ const {
   updateUserScore,
   getHighScoreUser,
   getRecentQuizResult,
+  getMonthlyQuizStats,
 } = require("../controllers/quizResultController");
 
 router.put("/update-score/:userId/:categoryId", updateUserScore);
 router.get("/top-players", getHighScoreUser);
 router.get("/recent-activity/:userId", getRecentQuizResult);
+router.get("/monthly-stats", getMonthlyQuizStats);
 
 module.exports = router;
