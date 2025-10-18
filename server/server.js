c7b2ebd4ecfdffa77753e1f9cc47615e9f315766
Bereket <bereketdesalegn097@gmail.com>
2025-10-18 09:19:09 +0300

    make cors to accept from both

--- server/server.js
+++ server/server.js
@@ -12,12 +12,15 @@ const quizResultRoutes = require("./routes/quizResultRoutes");
 dotenv.config();
 connectDB();
 
-const URL = process.env.URL || "http://localhost:5173";
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: URL, credentials: true }));
+app.use(
+  cors({
+    origin: [process.env.URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
