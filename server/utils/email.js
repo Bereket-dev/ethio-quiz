3e7ea59c017c7d64fdca2ceccab4c063d6b10c85
Bereket <bereketdesalegn097@gmail.com>
2025-10-20 19:34:44 +0300

    initial smtp transporter utils

---
+++ server/utils/email.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async (receiver, htmlMessage) => {
+  // Create a transporter for SMTP
+  const transporter = nodemailer.createTransport({
+    host: "smtp.example.com",
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  await transporter.verify();
+  console.log("Server is ready to take our messages");
+
+  (async () => {
+    try {
+      const info = await transporter.sendMail({
+        from: "Ethio Quiz", // sender address
+        to: receiver, // list of receivers
+        subject: "Password Reset", // Subject line
+        text: "Password Reset?", // plain text body
+        html: { htmlMessage }, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    } catch (err) {
+      console.error("Error while sending mail", err);
+    }
+  })();
+};
+
+module.exports = sendMail;
