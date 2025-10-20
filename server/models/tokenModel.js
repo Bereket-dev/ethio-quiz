dd76fd18336bfced40ba6bda8445d5912cd9b748
Bereket <bereketdesalegn097@gmail.com>
2025-10-20 19:31:55 +0300

    initial model for token management

---
+++ server/models/tokenModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tokenHash: { type: String, required: true },
+    type: { type: String, required: true, enum: ["passwordReset"] },
+    expiresAt: { type: Date, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Token", tokenSchema);
