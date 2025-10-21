f0efa38d7bff5ea59a4f3e692b7d575529e7c8c5
Bereket <bereketdesalegn097@gmail.com>
2025-10-21 16:52:09 +0300

    change from nodemailer to resend

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,33 +1,18 @@
-const nodemailer = require("nodemailer");
+const Resend = require("resend");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const sendMail = async (receiver, htmlMessage) => {
   try {
-    // Create a transporter for SMTP
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // upgrade later with STARTTLS
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
+    const resend = await new Resend(process.env.RESEND_API_KEY);
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: receiver,
+      subject: "Password Reset",
+      html: htmlMessage,
     });
-
-    await transporter.verify();
-    console.log("Server is ready to take our messages");
-
-    const info = await transporter.sendMail({
-      from: "Ethio Quiz", // sender address
-      to: receiver, // list of receivers
-      subject: "Password Reset", // Subject line
-      text: "Password Reset?", // plain text body
-      html: htmlMessage, // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.error("Email sent successfully");
   } catch (err) {
     console.error("Error while sending mail", err);
   }
