efc178ba8e0f70ef61256e7743050160376110cc
Bereket <bereketdesalegn097@gmail.com>
2025-10-22 01:17:14 +0300

    add token validator middleware

---
+++ server/middleware/token.js
@@ -0,0 +1,16 @@
+const tokenValidator = async (req, res) => {
+  try {
+    const { token } = req.params;
+
+    const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+    const resetToken = await Token.findOne({ tokenHash });
+    if (!resetToken || resetToken.expiresAt < Date.now())
+      return res.status(400).json({ message: "Invalid or expired token!" });
+
+    res.status(200).json({ message: "Valid token!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { tokenValidator };
--- server/routes/tokenRoutes.js
+++ server/routes/tokenRoutes.js
@@ -5,8 +5,10 @@ const {
   forgotPassword,
   resetPassword,
 } = require("../controllers/tokenController");
+const { tokenValidator } = require("../middleware/token");
 
 router.post("/forgot-password", forgotPassword);
 router.get("/reset-password/:token", resetPassword);
+router.get("/validate/:token", tokenValidator);
 
 module.exports = router;
