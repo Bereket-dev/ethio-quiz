79abed7977a4cdf5c0f3c53a3bded29a32d80faf
Bereket <bereketdesalegn097@gmail.com>
2025-10-21 15:52:44 +0300

    fix error in dependence and structure

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,36 +1,36 @@
 const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const sendMail = async (receiver, htmlMessage) => {
-  // Create a transporter for SMTP
-  const transporter = nodemailer.createTransport({
-    host: "smtp.example.com",
-    port: 587,
-    secure: false, // upgrade later with STARTTLS
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+  try {
+    // Create a transporter for SMTP
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // upgrade later with STARTTLS
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
 
-  await transporter.verify();
-  console.log("Server is ready to take our messages");
+    await transporter.verify();
+    console.log("Server is ready to take our messages");
 
-  (async () => {
-    try {
-      const info = await transporter.sendMail({
-        from: "Ethio Quiz", // sender address
-        to: receiver, // list of receivers
-        subject: "Password Reset", // Subject line
-        text: "Password Reset?", // plain text body
-        html: { htmlMessage }, // html body
-      });
+    const info = await transporter.sendMail({
+      from: "Ethio Quiz", // sender address
+      to: receiver, // list of receivers
+      subject: "Password Reset", // Subject line
+      text: "Password Reset?", // plain text body
+      html: htmlMessage, // html body
+    });
 
-      console.log("Message sent: %s", info.messageId);
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    } catch (err) {
-      console.error("Error while sending mail", err);
-    }
-  })();
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (err) {
+    console.error("Error while sending mail", err);
+  }
 };
 
 module.exports = sendMail;
