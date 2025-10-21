382fc1328aff685c1b519af670aade5dd0c29e26
Bereket <bereketdesalegn097@gmail.com>
2025-10-21 17:00:25 +0300

    using resend error fix

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,15 +4,15 @@ dotenv.config();
 
 const sendMail = async (receiver, htmlMessage) => {
   try {
-    const resend = await new Resend(process.env.RESEND_API_KEY);
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
-    await resend.emails.send({
+    const email = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: receiver,
       subject: "Password Reset",
       html: htmlMessage,
     });
-    console.error("Email sent successfully");
+    console.error("Email sent successfully:", email.id);
   } catch (err) {
     console.error("Error while sending mail", err);
   }
