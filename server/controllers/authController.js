90600fca0bde46ecf6bab05a3e2c3c5364010ea1
Bereket <bereketdesalegn097@gmail.com>
2025-10-24 16:34:58 +0300

    add error handling in login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,58 +33,67 @@ const signUpUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) return res.status(404).json({ error: "User not found!" });
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ error: "User not found!" });
 
-  if (!user.isVerified) {
-    const confirmEmail = await sendConfirmationEmail(email);
-    if (!confirmEmail)
-      return res.status(500).json({ message: confirmEmail.message });
+    if (!user.isVerified) {
+      const confirmEmail = await sendConfirmationEmail(email);
+      if (!confirmEmail)
+        return res.status(500).json({ message: confirmEmail.message });
 
-    return res.status(403).json({
-      message:
-        "Your email is not verified. A verification email has been sent.",
-    });
-  }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ error: "Invalid Credentials!" });
-
-  const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "1h",
+      return res.status(403).json({
+        message:
+          "Your email is not verified. A verification email has been sent.",
+      });
     }
-  );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-  });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ error: "Invalid Credentials!" });
 
-  res.json({
-    message: "Logged in successfully",
-    user: {
-      id: user._id,
-      username: user.username,
-      role: user.role,
-      email: user.email,
-    },
-  });
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+
+    res.json({
+      message: "Logged in successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        role: user.role,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const logoutUser = async (req, res) => {
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-  });
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    });
 
-  return res.status(200).json({ message: "Logged out!" });
+    return res.status(200).json({ message: "Logged out!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = { signUpUser, loginUser, logoutUser };
