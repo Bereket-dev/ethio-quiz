9bf214acda62218a8bdb19cdece6e1603bf2967c
Bereket <bereketdesalegn097@gmail.com>
2025-10-25 10:36:09 +0300

    remove link not found

--- client/src/Data/NavList.js
+++ client/src/Data/NavList.js
@@ -2,6 +2,4 @@ export const navList = [
   { name: 'Home', to: '/' },
   { name: 'Quiz', to: '/quiz' },
   { name: 'Leader Board', to: '/leaderboard' },
-  { name: 'Fresh Man', to: '/freshman' },
-  { name: 'Trivia', to: '/trivia' },
 ]
