d1c247a27ef5d44ece4cca5ae4d1296928c119de
Bereket <bereketdesalegn097@gmail.com>
2025-10-25 12:52:55 +0300

    move fetch kingdoms to parent

--- client/src/components/user/quiz/QuizKingdoms.jsx
+++ client/src/components/user/quiz/QuizKingdoms.jsx
@@ -1,22 +1,6 @@
 import QuizKingdomCard from './QuizKingdomCard'
-import { getKingdomList } from '../../../services/kingdomServices'
-import { useEffect, useState } from 'react'
 
-function QuizKingdoms({ title }) {
-  const [kingdomList, setKingdomList] = useState([])
-
-  useEffect(() => {
-    const storedKingdoms = JSON.parse(localStorage.getItem('kingdoms'))
-    if (Array.isArray(storedKingdoms) && storedKingdoms.length > 0)
-      setKingdomList(storedKingdoms)
-
-    async function fetchKingdoms() {
-      const result = await getKingdomList()
-      if (Array.isArray(result) && result.length > 0) setKingdomList(result)
-    }
-    fetchKingdoms()
-  }, [])
-
+function QuizKingdoms({ title, kingdomList = [] }) {
   return (
     kingdomList.length > 0 && (
       <div className="mx-auto mt-[100px] max-w-6xl px-14">
