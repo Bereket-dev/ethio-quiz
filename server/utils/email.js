d41f66355dc946c3a32e9ca288fcecef8c5a3a34
Bereket <bereketdesalegn097@gmail.com>
2025-10-28 22:13:13 +0300

    fix:email-verification-and-password-reset

--- server/utils/email.js
+++ server/utils/email.js
@@ -2,17 +2,23 @@ const { Resend } = require("resend");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const sendMail = async (receiver, htmlMessage) => {
+const sendMail = async (receiver, htmlMessage, emailReason) => {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
-    const email = await resend.emails.send({
-      from: "bereketdesalegn097@gmail.com",
+    const { data, error } = await resend.emails.send({
+      from: "Ethio quiz <noreply@ethioquiz.com.et>",
       to: receiver,
-      subject: "Password Reset",
+      subject: emailReason,
       html: htmlMessage,
     });
-    console.error("Email sent successfully:", email.id);
+
+    if (error) {
+      console.error("Email sending failed!", error);
+      return;
+    }
+
+    console.error("Email sent successfully:", data.id);
   } catch (err) {
     console.error("Error while sending mail", err);
   }
