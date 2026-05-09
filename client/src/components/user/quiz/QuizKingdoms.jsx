507037ea5031bbe73b327e51fbec6343f454ce5a
Bereket <bereketdesalegn097@gmail.com>
2026-05-09 22:32:22 +0300

    Update QuizKingdoms component layout and styles (#10)

--- client/src/components/user/quiz/QuizKingdoms.jsx
+++ client/src/components/user/quiz/QuizKingdoms.jsx
@@ -3,9 +3,9 @@ import QuizKingdomCard from './QuizKingdomCard'
 function QuizKingdoms({ title, kingdomList = [] }) {
   return (
     kingdomList.length > 0 && (
-      <div className="mx-auto mt-14 flex h-full flex-col items-center justify-center gap-8 px-6 md:max-w-6xl md:flex-row md:justify-between md:gap-14 md:px-14">
-        <div className="animate-fade-up flex flex-col items-center text-center md:block md:w-[55%] md:text-start">
-          <h2 className="pb-3 text-xl leading-snug font-semibold md:text-4xl md:tracking-wide">
+      <div className="mx-auto mt-14 flex h-full flex-col items-center justify-center gap-8 px-6 md:max-w-6xl md:justify-between md:gap-14 md:px-14">
+        <div className="animate-fade-up flex flex-col items-center text-center md:block md:text-start">
+          <h2 className="pb-3 text-center text-xl leading-snug font-semibold md:text-4xl md:tracking-wide">
             {title}
           </h2>
           <div className="grid w-full grid-cols-1 gap-x-10 gap-y-12 sm:grid-cols-2 lg:justify-items-center">
